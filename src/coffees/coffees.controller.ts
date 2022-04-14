450f59cd93282de57f9002a8b28bdb1f1c195da7
Riley Seaburg <riley.seaburg@22enterprises.com>
2022-04-14 19:13:41 -0400

    Add Patch & Delete Methods

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -26,8 +34,53 @@ export class CoffeesController {
    * @description This method is used to create a coffee.
    */
   @Post()
-  @HttpCode(HttpStatus.GONE)
   create(@Body('name') name: string): string {
     return `This action adds a new coffee: ${name}`;
+  }
+
+  /**
+   * @name updateCoffee
+   * @param {string} id
+   * @param {string} name
+   * @returns A string with the id of the coffee and the name of the coffee.
+   * @description This method is used to update a coffee.
+   * @example
+   * PATCH /coffees/1
+   * {
+   *  "name": "Americano"
+   * }
+   * @example
+   * PATCH /coffees/2
+   * {
+   * "name": "Cappuccino"
+   * }
+   */
+  @Patch('/:id')
+  //   Param and body decorators are required in a PATCH method
+  update(@Param('id') id: string, @Body('name') name: string): string {
+    return `This action updates a #${id} coffee to ${name}`;
+  }
+
+  /**
+   * @name deleteCoffee
+   * @param {string} id
+   * @returns A string with the id of the coffee.
+   * @description This method is used to delete a coffee.
+   * @example
+   * DELETE /coffees/1
+   * {
+   * "name": "Americano"
+   * }
+   *
+   * @example
+   * DELETE /coffees/2
+   * {
+   * "name": "Cappuccino"
+   * }
+   *
+   */
+  @Delete('/:id')
+  remove(@Param('id') id: string): string {
+    return `This action removes a #${id} coffee`;
   }
 }
