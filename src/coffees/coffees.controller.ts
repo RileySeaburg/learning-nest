62d82bad68c6892ae056d542c80ec7c5bf0dc1c3
Riley Seaburg <riley.seaburg@22enterprises.com>
2022-04-14 07:44:36 -0400

    Create Method to access coffee by ID

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
   findAll(): string {
     return 'This action returns all coffees';
+  }
+
+  /**
+   * @name findCoffeeByID
+   * @description
+   * This method is used to find a coffee by its ID.
+   * @param params General Parameters for the request.
+   * @returns A string with the id of the coffee.
+   */
+  @Get('/:id')
+  findOne(@Param() params): string {
+    return `This action returns a #${params.id} coffee`;
   }
 }
