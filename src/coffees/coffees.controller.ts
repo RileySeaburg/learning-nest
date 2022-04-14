e1a71fcc7acdfcc85c1ec21db09fc7231fb3fd61
Riley Seaburg <riley.seaburg@22enterprises.com>
2022-04-14 07:57:07 -0400

    Added coffee creation method

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -17,5 +17,16 @@ export class CoffeesController {
   @Get('/:id')
   findOne(@Param('id') id: string): string {
     return `This action returns a #${id} coffee`;
+  }
+
+  /**
+   * @name createCoffee
+   * @param body General Parameters for the request.
+   * @returns A string with the id of the coffee and the name of the coffee.
+   * @description This method is used to create a coffee.
+   */
+  @Post()
+  create(@Body() body: any): string {
+    return `This action adds a new coffee: ${body.name}`;
   }
 }
